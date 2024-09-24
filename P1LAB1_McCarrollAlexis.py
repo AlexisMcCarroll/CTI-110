3ec9249007c17c837081b6379e2ddda4f5d4180a
AlexisMcCarroll <mccarroa0649@student.faytechcc.edu>
2024-09-24 14:21:19 -0400

    first upload

---
+++ P1LAB1_McCarrollAlexis.py
@@ -0,0 +1,16 @@
+# Alexis McCarroll
+#9/24/2024
+#P1LAB1
+#Get input from the user and display output.
+
+# Get first name from user
+
+first_name = input ("Enter your first name: ")
+
+#Get the last name from user
+
+last_name = input ("Enter your last name: ")
+
+#display info to the user
+
+print("Hello," ,first_name, last_name + "!", "Welcome to CTI-110")
