25845d7c27afb0f3d5a958d648586a987bab5cd5
AlexisMcCarroll <mccarroa0649@student.faytechcc.edu>
2024-11-12 18:54:24 +0000

    first upload

---
+++ P4HW_Example.py
@@ -0,0 +1,27 @@
+# Example similar to P4HW1 
+
+# List of available items (this is not needed in hw)
+availableitems = ["litter", "cat food", "feather", "collar", "toy", \
+                  "litter box", "flea meds", "treats"]
+
+# get number of items to purchase
+numitems = int(input("How many items will you purchase? "))
+
+# Empty list to hold valid responses
+cart = []
+
+# Loop to get the items
+for item in range(numitems):
+    thisitem = input(f"Enter item #{item + 1}: ")
+    # loop to ensure thisitem is in the availableItems
+    while thisitem not in availableitems:
+        print(f"{thisitem} is not in stock!")
+        thisitem = input(f"Enter item #{item + 1} again: ")
+    # add tje valid item to the cart
+    cart.append (thisitem)
+
+# Loop to print each item in the cart
+print()
+print("Items we purchased are: ")
+for product in cart:
+    print(product)
