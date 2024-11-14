88fc725028e3b62ac73eecc24cbe9136cf43afec
AlexisMcCarroll <mccarroa0649@student.faytechcc.edu>
2024-11-14 13:46:55 -0500

    Add files via upload

--- P4HW1_McCarrollAlexis.py
+++ P4HW1_McCarrollAlexis.py
@@ -11,11 +11,11 @@ grades = []
 
 #loop to get the grades
 for grade in range(numgrades):
-    thisgrade = int(input(f"Enter score #{grade + 1}: "))
+    thisgrade = float(input(f"Enter score #{grade + 1}: "))
     #loop to ensure thisgrade is valid
     while thisgrade < 0 or thisgrade > 100:
         print(f"[thisgrade] INVALID Score entered!!!!")
-        thisgrade = int(input(f"Enter grade #{grade + 1} again: "))
+        thisgrade = float(input(f"Enter grade #{grade + 1} again: "))
     #add the valid grades to the cart
     grades.append (thisgrade)
 
@@ -23,7 +23,7 @@ for grade in range(numgrades):
 print()
 print("--------------Results-------------")
 # get the lowest score
-print(f"{'Lowest Grade: ':<15}{min(grades)}")
+print(f"{'Lowest Grade: ':<15}{min(grades):.1f}")
 #modify the list
 grades.remove (min(grades))
 print(f"{'Modified List: ':<15}{(grades)}")
@@ -44,4 +44,4 @@ elif average <= 59:
     letter_grade =  "F"
 
 print(f"{'Grade: ':<15}{letter_grade}")
-print("-" *30)
+print("-" *35)
