250387a811cfc2ac6a7ad5ec90d0ce9d1168ac6a
AlexisMcCarroll <mccarroa0649@student.faytechcc.edu>
2024-12-12 01:35:33 -0500

    Add files via upload

---
+++ P2LAB1_McCarrollAlexis.py
@@ -0,0 +1,32 @@
+# Alexis McCarroll
+# 10-8-2024
+# P2LAB1
+# Using Python's built in math libraries
+
+# Import the math library
+import math
+
+print(f"The value of pi is {math.pi}\n")
+
+# Get radius from user
+radius = float (input("What is the radius of the circle? "))
+print()
+
+# Calculate the diameter
+diameter = 2 * radius
+
+# Display diameter with one decimal place
+print(f"The diameter of the circle is {diameter:.1f}\n")
+
+# Calculate the circumfrence 
+circumf = 2 * math.pi * radius
+
+
+# Display the curcumfrence
+print(f"The circumfrence of the circle is {circumf:.2f}\n")
+
+# Calculate the area
+area = math.pi * math.pow(radius, 2)
+
+# Display the area
+print(f"The area of the circle is {area:.3f}\n")
